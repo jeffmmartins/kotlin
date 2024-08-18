719daf5dece163a3a0834c6f907e124531445f11
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-08-17 22:59:21 -0300

    variaveis mutavei e imutaveis

---
+++ src/variaveis.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var a: Int = 19
+    var b: String = "Jefferson"
+
+    a++
+    b+= "Martins"
+
+    println("numero ${a} e nome: ${b}")
+
+    var c: Boolean = (false)
+
+    println(c)
+}
