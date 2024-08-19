c56b0a2eb21ab470f11f3540cee00583fee13a32
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-08-19 18:43:42 -0300

    exemplo de estrutura de repetição while

---
+++ src/estrutura_de_repetição_while.kt
@@ -0,0 +1,19 @@
+fun main() {
+    var i = 0
+    while (i<10) {
+        println("${i }")
+        i++
+    }
+    print("\n")
+    while (i>0){
+        print("${i}")
+        i--
+    }
+
+    var str = "curso show"
+    var j=0
+    while (j<str.length){
+        print("${j} ")
+        j++
+    }
+}
