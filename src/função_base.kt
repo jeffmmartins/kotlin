13b7275879c412f703319c95c4e7ee3c8f3a3f7e
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-08-21 19:48:52 -0300

    exemplo de função com parametro e sem retorno

--- src/função_base.kt
+++ src/função_base.kt
@@ -10,4 +10,9 @@ fun main() {
     var x = 10
     var y = 20
     print(Soma(x,y))
+    soma_2(x,y)
+}
+
+fun soma_2(a:Int, b:Int) { // função com parametro e sem retorno
+    println("a soma é: ${a+b}" )
 }
