d7901f5ae47e418013737593350dd0468e0858c7
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-08-21 20:02:26 -0300

    chamando uma função dentro da outra

---
+++ src/casa.kt
@@ -0,0 +1,27 @@
+fun Casa(){
+print("passando pela casa")
+    quarto()
+}
+
+fun quarto(){
+    print("passando pelo quarto")
+    guardaroupa()
+}
+
+fun guardaroupa(){
+    print("passando pelo guarda- roupa")
+    sapato()
+}
+
+fun sapato(){
+    print("pescolhendo sapato")
+    cadarco()
+}
+
+fun cadarco(){
+    print("amarrando o sapato")
+}
+
+fun main() {
+Casa()
+}
