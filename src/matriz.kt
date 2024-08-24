5619e7f3d7baaeefe56fe5775299c10c8f6eecbe
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-08-24 18:53:59 -0300

    exemplos de matriz

--- src/matriz.kt
+++ src/matriz.kt
@@ -1,6 +1,6 @@
 fun main() {
     val linhas = 3
-    val colunas = 3
+    val colunas = 6
     // criando a matriz
     val matriz: Array<Array<Int>> = Array(linhas) {Array(colunas) {0} }
     var digito = -1
@@ -10,4 +10,19 @@ fun main() {
         }
         println()
     }
+    for (i in 0 until linhas) {
+        for (j in 0 until colunas){
+            matriz[i][j] = digito++
+        }
+        println()
+    }
+    println("impressão da matriz: ")
+    for (i in 0 until linhas) {
+        for (j in 0 until colunas){
+            print("${matriz[i][j]}")
+        }
+        println()
+    }
+    println("quantidade de linhas: ${matriz.size} ")
+    println("quantidade de colunas: ${matriz[0].size} ")
 }
