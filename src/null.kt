2ab0efea4c5df61db62240f9205f39195fb7feb0
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-08-31 21:36:39 -0300

    comentando o tratemento de exceção quando eu assumo o erro

--- src/null.kt
+++ src/null.kt
@@ -2,4 +2,5 @@ fun main() {
     // ? é indicando a exceção
     var str: String? = null
     println(str?.length) // kotlin identifica e assum eo tratamento do erro
+    println(str!!.length) // EU assumo o erro
 }
