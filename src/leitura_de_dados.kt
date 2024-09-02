589ff23ebdeabeb3e112e43027015e85f7278c0c
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-09-02 18:13:46 -0300

    exemplos de leitura de dados

--- src/leitura_de_dados.kt
+++ src/leitura_de_dados.kt
@@ -1,5 +1,13 @@
 fun main() {
-    print("Digite um número: ")
-    var s = readln()
-    print(s)
+
+
+    var num = readlnOrNull()?.toInt() ?: 0
+    var num1 = readlnOrNull()?.toDouble() ?: 0.0
+    var num2 = readlnOrNull()?.toFloat() ?: 0.0f
+    var num3 = readlnOrNull().toBoolean() ?: false
+    print("\n")
+    println("tipo Inteiro: ${num}")
+    println("tipo Double: ${num1}")
+    println("tipo Float: ${num2}")
+    println("tipo Boolean: ${num3}")
 }
