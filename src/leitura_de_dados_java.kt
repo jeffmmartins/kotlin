aa887230a6af26ae61161ec048baf2b2800192f1
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-09-03 17:58:42 -0300

    exemplo de leitura de dados tipo double

--- src/leitura_de_dados_java.kt
+++ src/leitura_de_dados_java.kt
@@ -7,6 +7,14 @@ fun ler_int() {
     println("o numero é: ${num  + 1}")
 }
 
+fun ler_double() {
+    val scan = Scanner(System. `in`)
+    println("Digite o numero tipo double: ")
+    var num = scan.nextDouble()
+    println("o numero é: ${num  + 1}")
+}
+
 fun main() {
  ler_int()
+ ler_double()
 }
