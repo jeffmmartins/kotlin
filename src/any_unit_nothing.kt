c1418b7fa49c20b57a8b950f708be39d7ba7cce5
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-09-07 17:45:21 -0300

    exemplo com o comando unit

--- src/any_unit_nothing.kt
+++ src/any_unit_nothing.kt
@@ -1,7 +1,12 @@
 fun main() {
     a(valor = 10)
+    b()
 }
 
 fun a(valor: Any){
     print("O valor da variavel tipo any é: ${valor}")
+}
+
+fun b(): Unit{
+    print("retorno de uma função void")
 }
