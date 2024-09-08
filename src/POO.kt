e30ecfa81a70137a7dbd82be91ff8b6fa252281c
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-09-07 21:32:10 -0300

    criando atributos e metodos

--- src/POO.kt
+++ src/POO.kt
@@ -1,8 +1,14 @@
 class Pessoa (val ano_nasc: Int, var nome: String){
-
+    var idade = 2023 - ano_nasc
+    fun saudacao(){
+        println("Seja bem vindo ${this.nome}")
+    }
 }
 
 fun main() {
     var pessoa: Pessoa = Pessoa(ano_nasc = 1988, nome = "Jefferson")
     println(pessoa.nome)
+    println(pessoa.ano_nasc)
+    println(pessoa.idade)
+    pessoa.saudacao()
 }
