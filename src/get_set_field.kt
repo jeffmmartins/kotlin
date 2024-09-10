a78f2319ee4d2efdcc550c6dbe3646b574dc1bfb
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-09-10 19:40:14 -0300

    exempÃÃlo com GET

---
+++ src/get_set_field.kt
@@ -0,0 +1,14 @@
+class Planeta(var nome: String){
+    private var id = 1
+    var tamanho = 1000
+    var fala = "Terra"
+        get() {
+            println("acessando o GET")
+            return field
+        }
+
+}
+
+fun main() {
+
+}
